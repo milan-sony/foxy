c0ff101b310235e9d27f2382ae5b1193b4c7cfe4
Milan Sony <milanfrom2001@gmail.com>
2023-07-03 00:49:05 +0530

    updated

--- web/script.js
+++ web/script.js
@@ -1,9 +1,18 @@
 function search(){
     let searchURL = document.getElementById("url").value;
-    console.log(searchURL)
     // call python function
     eel.proxy(searchURL)(response_back)
+    alert("Please wait till the chrome opens\n 🔍URL: "+searchURL)
 }
+
+let searchURL = document.getElementById("url");
+searchURL.addEventListener('keyup', (e) =>{
+    if(e.keyCode === 13){
+        // call python function
+        eel.proxy(searchURL.value)(response_back)
+        alert("Please wait till the chrome opens\n 🔍URL: "+searchURL)
+    }
+})
 
 function response_back(response){
     document.getElementById("res").innerHTML = "Last Visited URL: "+response;
